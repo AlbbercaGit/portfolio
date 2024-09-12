354d7b2e1e3202296f424f604767887fb20a8b45
AlbbercaGit <alejandroalbbercabarcelona@gmail.com>
2024-09-12 14:10:54 +0200

    Prepare for GitHub Pages deployments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,16 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', 
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        courier: ['Courier Prime', 'monospace'], 
-        lora: ['Lora Variable', 'serif'],
-        inter: ['"Inter Variable"', 'sans-serif'], 
+    extend: {fontFamily: {
+      courier: ['Courier Prime', 'monospace'], 
+      lora: ['Lora Variable', 'serif'],
+      inter: ['"Inter Variable"', 'sans-serif'], 
 
-      },
-    },
+    },},
   },
   plugins: [],
-}
-
+};
